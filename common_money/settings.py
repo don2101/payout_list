b33825378f33ae8b9a9a393d9762e6c90f34a34c
염희돈 <don210198@gmail.com>
2019-08-05 22:58:15 +0900

    Payout model definition & migrate

--- common_money/settings.py
+++ common_money/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'common_money.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- table/models.py
+++ table/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Payout(models.Model):
+    buyer = models.CharField(max_length=50, blank=False)
+    item = models.CharField(max_length=1000, blank=False)
+    money = models.IntegerField(blank=False)
+    buy_date = models.DateField(blank=False)
+    
