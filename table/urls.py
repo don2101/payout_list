49ed5419bbd9198f42c8c9691c6051c206cb991d
염희돈 <don210198@gmail.com>
2019-08-05 23:29:10 +0900

    main, add page 정의

--- table/urls.py
+++ table/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.main_page),
+    path('add_payout/', views.add_payout),
 ]
--- table/views.py
+++ table/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Payout
+from .forms import PayoutModelForm
+
 
 # Create your views here.
+def main_page(request):
+    payout_list = Payout.objects.all()
+    context = { 'payout_list': payout_list }
+
+    return render(request, 'table/items.html', context=context)
+
+
+def add_payout(request):
+    forms = PayoutModelForm()
+    context = { 'forms': forms }
+
+    return render(request, 'table/add.html', context=context)
+
+
